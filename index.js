fea5b9c44000dfa7a98d6e22e714abef1c9ab37c
Aditya5489 <ap5663019@gmail.com>
2025-09-11 19:43:47 +0530

    Deployment changes

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ dotenv.config();
 const app = express();
 
 const server=http.createServer(app);
-const io=new Server(server);
+const io=new Server(server,{
+  cors: {
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  }
+});
 
 const userSocketMap={};
 
@@ -107,7 +112,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 

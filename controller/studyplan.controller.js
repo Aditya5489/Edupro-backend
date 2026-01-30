62339b8e7737eee978489486375f602c54affdb8
Aditya5489 <ap5663019@gmail.com>
2026-01-30 18:56:40 +0530

    Update studyplan controller

--- controller/studyplan.controller.js
+++ controller/studyplan.controller.js
@@ -10,8 +10,9 @@ dotenv.config();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash" 
+  model: "models/gemini-pro",
 });
+
 
 const generatePlan = async (req, res) => {
   try {
